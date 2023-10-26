95d056ecfe0030fa220f5cb3252582215b6b8d08
admin <admin@bigdata.myguest.virtualbox.org>
2023-10-26 19:45:23 +0800

    ffijifj

---
+++ src/test/java/map1/AppTest.java
@@ -0,0 +1,20 @@
+package map1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
